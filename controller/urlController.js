7bd2c81545bd5a9ea877a6a0a2831742794e0afd
Priyanka-Tripathi98 <priyankatripa35@gmail.com>
2026-04-07 07:31:07 +0530

    set

--- controller/urlController.js
+++ controller/urlController.js
@@ -11,7 +11,7 @@ exports.createShortUrl = async (req, res) => {
     }
 
     const shortCode = generateCode();
- const shortUrl = `https://url-backend.onrender.com/url/${shortCode}`;
+ const shortUrl = `https://url-backend-cijf.onrender.com/url/${shortCode}`;
 
     await URL.create({
       originalUrl,
